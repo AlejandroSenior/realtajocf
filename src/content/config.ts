85feef6bce11ae21e97e79428eefc441b647f623
AlejandroSenior <alejandrosenior@outlook.es>
2025-10-15 20:41:05 +0200

    feat: Implement image gallery from cloudinary with carousels and modal functionality

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection } from 'astro:content';
+import { cldAssetsLoader } from 'astro-cloudinary/loaders';
+
+export const collections = {
+  assets: defineCollection({
+    loader: cldAssetsLoader({
+      limit: 100,
+      folder: 'realtajocf'
+    })
+  }),
+}
